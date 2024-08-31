aaa0ff59007f928e5e97fb36755966430d44aba0
AlejoAlesandria <alealesandria@gmail.com>
2024-08-31 16:35:21 -0300

    Agregado codigo MatLab

--- Matlab/Identificación/Final_Identification/Generacion_Funcion_transferencia.m
+++ Matlab/Identificación/Final_Identification/Generacion_Funcion_transferencia.m
@@ -1,5 +1,12 @@
-load("funcion_transferencia.mat");
+load("SS_o3.mat");
 
 [NUM, DEN] = ss2tf(ss2.A, ss2.B, ss2.C, ss2.D);
 
-H = tf(NUM, DEN)
+H_d = tf(NUM, DEN, 0.01);
+
+H_c = d2c(H_d)
+
+NUM = [-0.1128 0];
+DEN = [1 0.3731 77.44 -0.02801];
+
+HH = tf(NUM,DEN)
---
+++ Matlab/Identificación/Final_Identification/Untitled.m
@@ -0,0 +1,4 @@
+prbs = csvread('prbs_sequence_final.csv');
+output = readtable('output_final.csv');
+matriz = table2array(output);
+
